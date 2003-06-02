f89ad84742e442e6eb63b2a4901db3614965ff24
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2003-06-02 12:00:35 +0000

    - changed variable name from $databaseURL to $databaseBaseURL - generalized customization info and provided some examples

--- trunk/refbase/code/inc/ini.inc.php
+++ trunk/refbase/code/inc/ini.inc.php
@@ -3,20 +3,26 @@
 	// It contains variables that are common to all scripts and whose values can/should be customized.
 	// I.e., you can adjust their values as needed but you must not change the variable names themselves!
 
-	/*
-	Code adopted from example code by Hugh E. Williams and David Lane, authors of the book
-	"Web Database Application with PHP and MySQL", published by O'Reilly & Associates.
-	*/
-
 	// --------------------------------------------------------------------
 
-	$officialDatabaseName = "IP… Literature Database"; // the official name of this literature database
-	$databaseURL = "http://localhost/refs/index.php"; // the home page of this literature database
-	$adminLoginEmail = "ipoelit@ipoe.uni-kiel.de"; // the email address of the database admin
+	$officialDatabaseName = "INSTITUTION_NAME Literature Database"; // the official name of this literature database
+																	// e.g. "IPOE Literature Database"
 
-	$hostInstitutionName = "Institut fŸr Polarškologie"; // the full name of the institution hosting this literature database
-	$hostInstitutionAbbrevName = "IP…"; // the abbreviated name of the institution hosting this literature database
-	$hostInstitutionURL = "http://www.uni-kiel.de/ipoe/"; // the URL of the institution hosting this literature database
+	$databaseBaseURL = "http://YOUR_SERVER_ADDRESS/PATH_ON_SERVER/"; // the base url for this literature database (i.e., the URL to the root directory)
+																	// (IMPORTANT: the base url MUST end with a slash!)
+																	// e.g. "http://polaris.ipoe.uni-kiel.de/refs/"
+
+	$adminLoginEmail = "ADMIN_EMAIL_ADDRESS"; // the admin email address (by which a user is granted admin status after successful login!)
+											// e.g. "ipoelit@ipoe.uni-kiel.de"
+
+	$hostInstitutionName = "INSTITUTION_FULL_NAME"; // the full name of the institution hosting this literature database
+													// e.g. "Institute for Polar Ecology, Kiel Germany"
+
+	$hostInstitutionAbbrevName = "INSTITUTIONAL_ABBREVIATION"; // the abbreviated name of the institution hosting this literature database
+																// e.g. "IPOE"
+
+	$hostInstitutionURL = "INSTITUTION_WEB_ADDRESS"; // the URL of the institution hosting this literature database
+													// e.g. "http://www.uni-kiel.de/ipoe/"
 
 	// --------------------------------------------------------------------
 ?>
