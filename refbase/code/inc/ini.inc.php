d9cccc3815816c18e8644e2e12b98ce41755e1cc
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2003-06-30 18:55:53 +0000

    - added '$addNewUsers' variable: by setting its value to 'everyone' any visitor who isn't logged in will be allowed to add a new user   Note that this feature is actually only meant to add the very first user to the users table.   After you've done so, it is highly recommended to change the value of '$addNewUsers' to 'admin'!

--- refbase/code/inc/ini.inc.php
+++ refbase/code/inc/ini.inc.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./ini.inc.php
 	// Created:    12-Jan-03, 17:58
-	// Modified:   2-Jun-03, 13:44
+	// Modified:   30-Jun-03, 15:16
 
 	// This is the customization include file.
 	// It contains variables that are common to all scripts and whose values can/should be customized.
@@ -20,11 +20,19 @@
 																	// (IMPORTANT: the base url MUST end with a slash!)
 																	// e.g. "http://polaris.ipoe.uni-kiel.de/refs/"
 
+	$addNewUsers = "everyone"; // specifies who'll be allowed to add a new user to the users table (possible values are: "everyone", "admin")
+								// VERY IMPORTANT NOTE: the value is set to "everyone" by default so that you'll be able to setup the very first user!
+								// Its best to set up the admin as the first user (and specify his email address below!), then change the value of
+								// $addNewUsers to "admin". By that you prevent other users to be able to mess with your users table.
+
 	$adminLoginEmail = "ADMIN_EMAIL_ADDRESS"; // the admin email address (by which a user is granted admin status after successful login!)
-											// e.g. "ipoelit@ipoe.uni-kiel.de"
+											// e.g. "admin@ipoe.uni-kiel.de"
+
+	$feedbackEmail = "FEEDBACK_EMAIL_ADDRESS"; // the feedback email address to which any support questions or suggestions should be sent
+												// e.g. "ipoelit@ipoe.uni-kiel.de"
 
 	$hostInstitutionName = "INSTITUTION_FULL_NAME"; // the full name of the institution hosting this literature database
-													// e.g. "Institute for Polar Ecology, Kiel Germany"
+													// e.g. "Institute for Polar Ecology"
 
 	$hostInstitutionAbbrevName = "INSTITUTIONAL_ABBREVIATION"; // the abbreviated name of the institution hosting this literature database
 																// e.g. "IPOE"
