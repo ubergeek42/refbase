198bd2d694a1e9263ca1e933f906681c4b30e929
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2003-09-06 16:27:56 +0000

    - we only append user specific fields to any sql query if a user is logged in - if a user is logged in, the FROM statement of any sql query now includes the   necessary 'LEFT JOIN user_data...' part in order to join columns from both tables

--- trunk/refbase/code/php/receipt.php
+++ trunk/refbase/code/php/receipt.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./receipt.php
 	// Created:    2-Jan-03, 22:43
-	// Modified:   19-Jan-03, 17:20
+	// Modified:   03-Sep-03, 21:47
 
 	// This php script will display a feedback page after any action of
 	// adding/editing/deleting a record. It will display links to the
@@ -67,8 +67,13 @@
 
 	// (4b) DISPLAY results:
 	// First, construct the correct sql query that will link back to the added/edited record:
-	$sqlQuery = "SELECT author, title, year, publication, abbrev_journal, volume, issue, pages, address, corporate_author, keywords, abstract, publisher, place, editor, language, summary_language, orig_title, series_editor, series_title, abbrev_series_title, series_volume, series_issue, edition, issn, isbn, medium, area, expedition, conference, location, call_number, reprint_status, marked, approved, file, serial, type, notes, user_keys, user_notes";
-	$sqlQuery .= " FROM refs WHERE serial RLIKE \"^" . $serialNo . "$\" ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
+	if (session_is_registered("loginEmail")) // if a user is logged in, show user specific fields:
+		$sqlQuery = "SELECT author, title, type, year, publication, abbrev_journal, volume, issue, pages, address, corporate_author, keywords, abstract, publisher, place, editor, language, summary_language, orig_title, series_editor, series_title, abbrev_series_title, series_volume, series_issue, edition, issn, isbn, medium, area, expedition, conference, notes, file, location, approved, call_number, serial, marked, copy, user_file, user_keys, user_notes"
+				. " FROM refs LEFT JOIN user_data ON serial = record_id AND user_id =" . $loginUserID . " WHERE serial RLIKE \"^(" . $serialNo . ")$\" ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
+	else // if NO user logged in, don't display any user specific fields:
+		$sqlQuery = "SELECT author, title, type, year, publication, abbrev_journal, volume, issue, pages, address, corporate_author, keywords, abstract, publisher, place, editor, language, summary_language, orig_title, series_editor, series_title, abbrev_series_title, series_volume, series_issue, edition, issn, isbn, medium, area, expedition, conference, notes, file, location, approved, call_number, serial"
+				. " FROM refs WHERE serial RLIKE \"^(" . $serialNo . ")$\" ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
+
 	$sqlQuery = rawurlencode($sqlQuery);
 	
 	// Second, we'll have to URL encode the sqlQuery part within '$oldQuery' while maintaining the rest unencoded(!):
