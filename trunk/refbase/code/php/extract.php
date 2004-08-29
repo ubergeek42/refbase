4d994ad4f465a5a4e14783cb5c4a8f2ecce624a8
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2004-08-29 21:27:37 +0000

    - sessions work now with 'register_globals = OFF'; plus: - uses now the 'start_session()' function from 'include.inc.php' - replaced "session_is_registered("...")" with "isset($_SESSION['...']" - replaced direct use of 'session_unregister()' function with   'deleteSessionVariable()' function from 'include.inc.php' - the wording of the page title & header text was changed slightly - added code to support the print view feature - changed string "Output Options" to "Display Options" - the user can now choose whether links should be displayed or not - modified some HTML code to make room for the new "Display Links" checkbox   and "View type" popup

--- trunk/refbase/code/php/extract.php
+++ trunk/refbase/code/php/extract.php
@@ -5,9 +5,9 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./extract.php
 	// Created:    29-Jul-02, 16:39
-	// Modified:   16-Nov-03, 21:32
+	// Modified:   20-May-04, 23:05
 
-	// Search formular that offers to extract
+	// Search form that offers to extract
 	// literature cited within a text and build
 	// an appropriate reference list from that.
 
@@ -24,48 +24,69 @@
 	
 	// --------------------------------------------------------------------
 
-	// Connect to a session
-	session_start();
-
-	// CAUTION: Doesn't work with 'register_globals = OFF' yet!!
+	// START A SESSION:
+	// call the 'start_session()' function (from 'include.inc.php') which will also read out available session variables:
+	start_session();
 
 	// --------------------------------------------------------------------
 
 	// If there's no stored message available:
-	if (!session_is_registered("HeaderString"))
-		$HeaderString = "Extract literature cited within a text and build an appropriate reference list:"; // Provide the default message
+	if (!isset($_SESSION['HeaderString']))
+		$HeaderString = "Extract citations from a text and build an appropriate reference list:"; // Provide the default message
 	else
-		session_unregister("HeaderString"); // Note: though we clear the session variable, the current message is still available to this script via '$HeaderString'
+	{
+		$HeaderString = $_SESSION['HeaderString']; // extract 'HeaderString' session variable (only necessary if register globals is OFF!)
+
+		// Note: though we clear the session variable, the current message is still available to this script via '$HeaderString':
+		deleteSessionVariable("HeaderString"); // function 'deleteSessionVariable()' is defined in 'include.inc.php'
+	}
+
+	// Extract the view type requested by the user (either 'Print', 'Web' or ''):
+	// ('' will produce the default 'Web' output style)
+	if (isset($_REQUEST['viewType']))
+		$viewType = $_REQUEST['viewType'];
+	else
+		$viewType = "";
 
 	// Show the login status:
 	showLogin(); // (function 'showLogin()' is defined in 'include.inc.php')
 
 	// (2a) Display header:
 	// call the 'displayHTMLhead()' and 'showPageHeader()' functions (which are defined in 'header.inc.php'):
-	displayHTMLhead(htmlentities($officialDatabaseName) . " -- Extract Cited Literature", "index,follow", "Search the " . htmlentities($officialDatabaseName), "", false, "");
+	displayHTMLhead(htmlentities($officialDatabaseName) . " -- Extract Citations", "index,follow", "Search the " . htmlentities($officialDatabaseName), "", false, "", $viewType);
 	showPageHeader($HeaderString, $loginWelcomeMsg, $loginStatus, $loginLinks, "");
 
 	// (2b) Start <form> and <table> holding the form elements:
 	echo "\n<form action=\"search.php\" method=\"POST\">";
+
 	echo "\n<input type=\"hidden\" name=\"formType\" value=\"extractSearch\">"
-		. "\n<input type=\"hidden\" name=\"submit\" value=\"Export\">" // provide a default value for the 'submit' form tag. Otherwise, some browsers may not recognize the correct output format when a user hits <enter> within a form field (instead of clicking the "Export" button)
-		. "\n<input type=\"hidden\" name=\"showLinks\" value=\"1\">"; // embed '$showLinks=1' so that links get displayed on any 'display details' page
+		. "\n<input type=\"hidden\" name=\"submit\" value=\"Export\">"; // provide a default value for the 'submit' form tag. Otherwise, some browsers may not recognize the correct output format when a user hits <enter> within a form field (instead of clicking the "Export" button)
+
 	echo "\n<table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"10\" width=\"95%\" summary=\"This table holds the search form\">"
 			. "\n<tr>\n\t<td width=\"58\" valign=\"top\"><b>Extract Citations From:</b></td>\n\t<td width=\"10\">&nbsp;</td>"
 			. "\n\t<td><textarea name=\"sourceText\" rows=\"6\" cols=\"60\">Paste your text here...</textarea></td>"
 			. "\n</tr>"
-			. "\n<tr>\n\t<td valign=\"top\"><b>Serial Delimiters:</b></td>\n\t<td>&nbsp;</td>"
+			. "\n<tr>\n\t<td valign=\"top\" rowspan=\"2\"><b>Serial Delimiters:</b></td>\n\t<td>&nbsp;</td>"
 			. "\n\t<td valign=\"top\">Specify the character(s) that enclose record serial numbers:</td>"
 			. "\n</tr>"
-			. "\n<tr>\n\t<td>&nbsp;</td>\n\t<td>&nbsp;</td>"
+			. "\n<tr>\n\t<td>&nbsp;</td>"
 			. "\n\t<td valign=\"top\">Start Delimiter:&nbsp;&nbsp;&nbsp;<input type=\"text\" name=\"startDelim\" value=\"{\" size=\"4\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;End Delimiter:&nbsp;&nbsp;&nbsp;<input type=\"text\" name=\"endDelim\" value=\"}\" size=\"4\"></td>"
 			. "\n</tr>"
-			. "\n<tr>\n\t<td valign=\"top\"><b>Output Options:</b></td>\n\t<td>&nbsp;</td>"
-			. "\n\t<td valign=\"top\">Show&nbsp;&nbsp;&nbsp;<input type=\"text\" name=\"showRows\" value=\"100\" size=\"4\">&nbsp;&nbsp;&nbsp;records per page</td>"
+			. "\n<tr>\n\t<td valign=\"top\" rowspan=\"2\"><b>Display Options:</b></td>\n\t<td>&nbsp;</td>"
+			. "\n\t<td valign=\"top\"><input type=\"checkbox\" name=\"showLinks\" value=\"1\" checked>&nbsp;&nbsp;&nbsp;Display Links"
+			. "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Show&nbsp;&nbsp;&nbsp;<input type=\"text\" name=\"showRows\" value=\"100\" size=\"4\">&nbsp;&nbsp;&nbsp;records per page</td>"
+			. "\n</tr>"
+			. "\n<tr>\n\t<td>&nbsp;</td>"
+			. "\n\t<td valign=\"top\">View type:&nbsp;&nbsp;"
+			. "\n\t\t<select name=\"viewType\">"
+			. "\n\t\t\t<option>Web</option>"
+			. "\n\t\t\t<option>Print</option>"
+			. "\n\t\t</select>"
+			. "\n\t</td>"
 			. "\n</tr>"
 			. "\n<tr>\n\t<td>&nbsp;</td>\n\t<td>&nbsp;</td>"
-			. "\n\t<td>\n\t\t<input type=\"submit\" name=\"submit\" value=\"Export\">&nbsp;&nbsp;&nbsp;"
-			. "\n\t\tin Format:&nbsp;&nbsp;"
+			. "\n\t<td>\n\t\t<br><input type=\"submit\" name=\"submit\" value=\"Export\">&nbsp;&nbsp;&nbsp;"
+			. "\n\t\treferences using style:&nbsp;&nbsp;"
 			. "\n\t\t<select name=\"exportFormatSelector\">"
 			. "\n\t\t\t<option>Polar Biol</option>"
 			. "\n\t\t\t<option>Mar Biol</option>"
@@ -98,5 +119,6 @@
 
 	// --------------------------------------------------------------------
 ?>
+
 </body>
 </html> 
