55fd2e1d390626cc1c2ebf785bed7381c0b786ef
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2004-09-29 23:48:52 +0000

    - sessions work now with 'register_globals = OFF'; plus: - uses now the 'start_session()' function from 'include.inc.php' - replaced "session_is_registered("...")" with "isset($_SESSION['...']" - replaced direct use of 'session_unregister()' function with   custom function 'deleteSessionVariable()' from 'include.inc.php' - replaced direct use of 'session_register()' function with   custom function 'saveSessionVariable()' from 'include.inc.php' - replaced "$HTTP_REFERER" with "$_SERVER['HTTP_REFERER']" - added code to support the print view feature - replaced direct use of functions 'mysql_connect()' & 'mysql_select_db()'   with custom function 'connectToMySQLDatabase()' from 'include.inc.php' - replaced direct use of 'mysql_query()' function with custom function   'queryMySQLDatabase()' from 'include.inc.php' - replaced direct use of 'mysql_close()' function with custom function   'disconnectFromMySQLDatabase()' from 'include.inc.php'

--- trunk/refbase/code/php/user_receipt.php
+++ trunk/refbase/code/php/user_receipt.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./user_receipt.php
 	// Created:    16-Apr-02, 10:54
-	// Modified:   16-Nov-03, 21:55
+	// Modified:   30-May-04, 19:35
 
 	// This script shows the user a receipt for their user UPDATE or INSERT.
 	// It carries out no database actions and can be bookmarked.
@@ -25,19 +25,28 @@
 
 	// --------------------------------------------------------------------
 
-	// Re-establish the existing session
-	session_start();
-	
-	// CAUTION: Doesn't work with 'register_globals = OFF' yet!!
+	// START A SESSION:
+	// call the 'start_session()' function (from 'include.inc.php') which will also read out available session variables:
+	start_session();
+
+	// Extract the 'userID' parameter from the request:
+	if (isset($_REQUEST['userID']))
+		$userID = $_REQUEST['userID'];
+	else
+		$userID = ""; // we do it for clarity reasons here (and in order to prevent any 'Undefined variable...' messages)
 
 	// Check if the user is logged in
-	if (!session_is_registered("loginEmail") && ($userID != 0))
+	if (!isset($_SESSION['loginEmail']) && ($userID != 0))
 	{
-		session_register("HeaderString"); // save an error message
+		// save an error message:
 		$HeaderString = "<b><span class=\"warning\">You must login to view your user account details!</span></b>";
 
-		session_register("referer"); // save the URL of the currently displayed page
-		$referer = $HTTP_REFERER;
+		// save the URL of the currently displayed page:
+		$referer = $_SERVER['HTTP_REFERER'];
+
+		// Write back session variables:
+		saveSessionVariable("HeaderString", $HeaderString); // function 'saveSessionVariable()' is defined in 'include.inc.php'
+		saveSessionVariable("referer", $referer);
 
 		header("Location: user_login.php");
 		exit;
@@ -46,32 +55,43 @@
 	// Check the correct parameters have been passed
 	if ($userID == "")
 	{
-		session_register("HeaderString"); // save an error message
+		// save an error message:
 		$HeaderString = "<b><span class=\"warning\">Incorrect parameters to script 'user_receipt.php'!</span></b>";
 
+		// Write back session variables:
+		saveSessionVariable("HeaderString", $HeaderString); // function 'saveSessionVariable()' is defined in 'include.inc.php'
+
 		// Redirect the browser back to the calling page
-		header("Location: index.php"); // Note: if 'header("Location: $HTTP_REFERER")' is used, the error message won't get displayed! ?:-/
+		header("Location: index.php"); // Note: if 'header("Location: " . $_SERVER['HTTP_REFERER'])' is used, the error message won't get displayed! ?:-/
 		exit;
 	}
 
 	// --------------------------------------------------------------------
 
+	// (1) OPEN CONNECTION, (2) SELECT DATABASE
+	connectToMySQLDatabase(""); // function 'connectToMySQLDatabase()' is defined in 'include.inc.php'
+
+	// --------------------------------------------------------------------
+
 	// For regular users, validate that the correct userID has been passed to the script:
- 	if (session_is_registered("loginEmail") && ($loginEmail != $adminLoginEmail))
+ 	if (isset($_SESSION['loginEmail']) && ($loginEmail != $adminLoginEmail))
 		// check this user matches the userID (viewing user account details is only allowed to the admin)
-		if ($userID != getUserID($loginEmail, NULL))
+		if ($userID != getUserID($loginEmail))
 		{
-			session_register("HeaderString"); // otherwise save an error message
+			// otherwise save an error message:
 			$HeaderString = "<b><span class=\"warning\">You can only view your own user receipt!<span></b>";
-	
-			$userID = getUserID($loginEmail, NULL); // and re-establish the user's correct user_id
+
+			// Write back session variables:
+			saveSessionVariable("HeaderString", $HeaderString); // function 'saveSessionVariable()' is defined in 'include.inc.php'
+
+			$userID = getUserID($loginEmail); // and re-establish the user's correct user_id
 		}
 
 	// Extract the type of action requested by the user, either 'delete' or ''.
 	// ('' or anything else will be treated equal to 'edit').
 	// We actually extract the variable 'userAction' only if the admin is logged in
 	// (since only the admin will be allowed to delete a user):
- 	if (session_is_registered("loginEmail") && ($loginEmail == $adminLoginEmail)) // ('$adminLoginEmail' is specified in 'ini.inc.php')
+ 	if (isset($_SESSION['loginEmail']) && ($loginEmail == $adminLoginEmail)) // ('$adminLoginEmail' is specified in 'ini.inc.php')
  	{
 		if (isset($_REQUEST['userAction']))
 			$userAction = $_REQUEST['userAction'];
@@ -80,11 +100,14 @@
 
 		if ($userAction == "Delete")
 		{
-			if ($userID == getUserID($loginEmail, NULL)) // if the admin userID was passed to the script
+			if ($userID == getUserID($loginEmail)) // if the admin userID was passed to the script
 			{
-				session_register("HeaderString"); // save an error message
+				// save an error message:
 				$HeaderString = "<b><span class=\"warning\">You cannot delete your own user data!<span></b>";
-		
+
+				// Write back session variables:
+				saveSessionVariable("HeaderString", $HeaderString); // function 'saveSessionVariable()' is defined in 'include.inc.php'
+
 				$userAction = "Edit"; // and re-set the user action to 'edit'
 			}
 		}
@@ -94,23 +117,14 @@
 	else // otherwise we simply assume an 'edit' action, no matter what was passed to the script (thus, no regular user will be able to delete a user)
 		$userAction = "Edit";
 
+	// Extract the view type requested by the user (either 'Print', 'Web' or ''):
+	// ('' will produce the default 'Web' output style)
+	if (isset($_REQUEST['viewType']))
+		$viewType = $_REQUEST['viewType'];
+	else
+		$viewType = "";
+
 	// --------------------------------------------------------------------
-
-	// (1) OPEN CONNECTION, (2) SELECT DATABASE, (3) RUN QUERY, (4) DISPLAY RECEIPT, (5) CLOSE CONNECTION
-
-	// (1) OPEN the database connection:
-	//      (variables are set by include file 'db.inc.php'!)
-	if (!($connection = @ mysql_connect($hostName, $username, $password)))
-		if (mysql_errno() != 0) // this works around a stupid(?) behaviour of the Roxen webserver that returns 'errno: 0' on success! ?:-(
-			showErrorMsg("The following error occurred while trying to connect to the host:", "");
-
-	// (2) SELECT the database:
-	//      (variables are set by include file 'db.inc.php'!)
-	if (!(mysql_select_db($databaseName, $connection)))
-		if (mysql_errno() != 0) // this works around a stupid(?) behaviour of the Roxen webserver that returns 'errno: 0' on success! ?:-(
-			showErrorMsg("The following error occurred while trying to connect to the database:", "");
-
-	// ----------------------------------------------
 
 	// Show the login status:
 	showLogin(); // (function 'showLogin()' is defined in 'include.inc.php')
@@ -124,9 +138,7 @@
 	// ----------------------------------------------
 
 	// (5) CLOSE the database connection:
-	if (!(mysql_close($connection)))
-		if (mysql_errno() != 0) // this works around a stupid(?) behaviour of the Roxen webserver that returns 'errno: 0' on success! ?:-(
-			showErrorMsg("The following error occurred while trying to disconnect from the database:", "");
+	disconnectFromMySQLDatabase(""); // function 'disconnectFromMySQLDatabase()' is defined in 'include.inc.php'
 
 	// --------------------------------------------------------------------
 
@@ -134,6 +146,7 @@
 	function showEmailConfirmation($userID)
 	{
 		global $HeaderString;
+		global $viewType;
 		global $loginWelcomeMsg;
 		global $loginStatus;
 		global $loginLinks;
@@ -142,13 +155,18 @@
 		global $officialDatabaseName;
 
 		// Build the correct header message:
-		if (!session_is_registered("HeaderString"))
+		if (!isset($_SESSION['HeaderString']))
 			$HeaderString = "Submission confirmation:"; // provide the default message
 		else
-			session_unregister("HeaderString"); // Note: though we clear the session variable, the current message is still available to this script via '$HeaderString'
+		{
+			$HeaderString = $_SESSION['HeaderString']; // extract 'HeaderString' session variable (only necessary if register globals is OFF!)
+
+			// Note: though we clear the session variable, the current message is still available to this script via '$HeaderString':
+			deleteSessionVariable("HeaderString"); // function 'deleteSessionVariable()' is defined in 'include.inc.php'
+		}
 
 		// Call the 'displayHTMLhead()' and 'showPageHeader()' functions (which are defined in 'header.inc.php'):
-		displayHTMLhead(htmlentities($officialDatabaseName) . " -- User Receipt", "noindex,nofollow", "Receipt page confirming correct submission of new user details to the " . htmlentities($officialDatabaseName), "", false, "");
+		displayHTMLhead(htmlentities($officialDatabaseName) . " -- User Receipt", "noindex,nofollow", "Receipt page confirming correct submission of new user details to the " . htmlentities($officialDatabaseName), "", false, "", $viewType);
 		showPageHeader($HeaderString, $loginWelcomeMsg, $loginStatus, $loginLinks, "");
 
 		$confirmationText = "Thanks for your interest in the " . htmlentities($officialDatabaseName) . "!"
@@ -172,6 +190,7 @@
 	function showUserData($userID, $userAction, $connection)
 	{
 		global $HeaderString;
+		global $viewType;
 		global $loginWelcomeMsg;
 		global $loginStatus;
 		global $loginLinks;
@@ -183,24 +202,27 @@
 		$query = "SELECT * FROM users WHERE user_id = $userID";
 
 		// (3) RUN the query on the database through the connection:
-		if (!($result = @ mysql_query($query, $connection)))
-			if (mysql_errno() != 0) // this works around a stupid(?) behaviour of the Roxen webserver that returns 'errno: 0' on success! ?:-(
-				showErrorMsg("Your query:\n<br>\n<br>\n<code>$query</code>\n<br>\n<br>\n caused the following error:", "");
+		$result = queryMySQLDatabase($query, ""); // function 'queryMySQLDatabase()' is defined in 'include.inc.php'
 
 		// (4) EXTRACT results (since 'user_id' is the unique primary key for the 'users' table, there will be only one matching row)
 		$row = @ mysql_fetch_array($result);
 
 		// Build the correct header message:
-		if (!session_is_registered("HeaderString")) // if there's no saved message
+		if (!isset($_SESSION['HeaderString'])) // if there's no saved message
 			if ($userAction == "Delete") // provide an appropriate header message:
 				$HeaderString = "<b><span class=\"warning\">Delete user</span> " . htmlentities($row["first_name"]) . " " . htmlentities($row["last_name"]) . " (" . $row["email"] . ")</b>:";
 			else // provide the default message:
 				$HeaderString = "Account details for <b>" . htmlentities($row["first_name"]) . " " . htmlentities($row["last_name"]) . " (" . $row["email"] . ")</b>:";
 		else
-			session_unregister("HeaderString"); // Note: though we clear the session variable, the current message is still available to this script via '$HeaderString'
+		{
+			$HeaderString = $_SESSION['HeaderString']; // extract 'HeaderString' session variable (only necessary if register globals is OFF!)
+
+			// Note: though we clear the session variable, the current message is still available to this script via '$HeaderString':
+			deleteSessionVariable("HeaderString"); // function 'deleteSessionVariable()' is defined in 'include.inc.php'
+		}
 
 		// Call the 'displayHTMLhead()' and 'showPageHeader()' functions (which are defined in 'header.inc.php'):
-		displayHTMLhead(htmlentities($officialDatabaseName) . " -- User Receipt", "noindex,nofollow", "Receipt page confirming correct entry of user details for the " . htmlentities($officialDatabaseName), "", false, "");
+		displayHTMLhead(htmlentities($officialDatabaseName) . " -- User Receipt", "noindex,nofollow", "Receipt page confirming correct entry of user details for the " . htmlentities($officialDatabaseName), "", false, "", $viewType);
 		showPageHeader($HeaderString, $loginWelcomeMsg, $loginStatus, $loginLinks, "");
 
 		// Start a table:
@@ -210,7 +232,7 @@
 		{
 			// Display a password reminder:
 			// (but only if a normal user is logged in -OR- the admin is logged in AND the updated user data are his own!)
-			if (($loginEmail != $adminLoginEmail) | (($loginEmail == $adminLoginEmail) && ($userID == getUserID($loginEmail, $connection))))
+			if (($loginEmail != $adminLoginEmail) | (($loginEmail == $adminLoginEmail) && ($userID == getUserID($loginEmail))))
 				echo "\n<tr>\n\t<td><i>Please record your password somewhere safe for future use!</i></td>\n</tr>";
 	
 			// Print title, first name, last name and institutional abbreviation:
@@ -277,7 +299,7 @@
 			echo "\n\t</td>\n</tr>";
 
 			// If the admin is logged in, display a button that will allow to edit/delete the currently shown user:
-			if (session_is_registered("loginEmail") && ($loginEmail == $adminLoginEmail)) // ('$adminLoginEmail' is specified in 'ini.inc.php')
+			if (isset($_SESSION['loginEmail']) && ($loginEmail == $adminLoginEmail)) // ('$adminLoginEmail' is specified in 'ini.inc.php')
 			{
 				if ($userAction == "Delete")
 					echo "\n<tr>\n\t<td>"
@@ -309,5 +331,6 @@
 
 	// --------------------------------------------------------------------
 ?>
+
 </body>
 </html>
