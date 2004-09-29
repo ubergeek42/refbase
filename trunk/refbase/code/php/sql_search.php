b260ce5176aae22335884721181885321560f2c9
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2004-09-29 23:07:30 +0000

    - sessions work now with 'register_globals = OFF'; plus: - uses now the 'start_session()' function from 'include.inc.php' - replaced "session_is_registered("...")" with "isset($_SESSION['...']" - replaced direct use of 'session_unregister()' function with   custom function 'deleteSessionVariable()' from 'include.inc.php' - 'Display SQL query' is now unchecked by default - added code to support the print view feature - changed string "Output Options" to "Display Options" - renamed all occurrences of 'export' to 'cite' to better reflect its purpose - added code to support user-specific permissions

--- trunk/refbase/code/php/sql_search.php
+++ trunk/refbase/code/php/sql_search.php
@@ -5,9 +5,9 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./sql_search.php
 	// Created:    29-Jul-02, 16:39
-	// Modified:   16-Nov-03, 21:49
+	// Modified:   29-Sep-04, 14:57
 
-	// Search formular that offers to specify a custom sql query.
+	// Search form that offers to specify a custom sql query.
 	// It offers some output options (like how many records to display per page)
 	// and provides some examples and links for further information on sql queries.
 
@@ -24,18 +24,29 @@
 
 	// --------------------------------------------------------------------
 
-	// Connect to a session
-	session_start();
-
-	// CAUTION: Doesn't work with 'register_globals = OFF' yet!!
+	// START A SESSION:
+	// call the 'start_session()' function (from 'include.inc.php') which will also read out available session variables:
+	start_session();
 
 	// --------------------------------------------------------------------
 
 	// If there's no stored message available:
-	if (!session_is_registered("HeaderString"))
+	if (!isset($_SESSION['HeaderString']))
 		$HeaderString = "Search the database by use of a SQL query:"; // Provide the default message
 	else
-		session_unregister("HeaderString"); // Note: though we clear the session variable, the current message is still available to this script via '$HeaderString'
+	{
+		$HeaderString = $_SESSION['HeaderString']; // extract 'HeaderString' session variable (only necessary if register globals is OFF!)
+
+		// Note: though we clear the session variable, the current message is still available to this script via '$HeaderString':
+		deleteSessionVariable("HeaderString"); // function 'deleteSessionVariable()' is defined in 'include.inc.php'
+	}
+
+	// Extract the view type requested by the user (either 'Print', 'Web' or ''):
+	// ('' will produce the default 'Web' output style)
+	if (isset($_REQUEST['viewType']))
+		$viewType = $_REQUEST['viewType'];
+	else
+		$viewType = "";
 
 	// Check if the script was called with parameters (like: 'sql_search.php?customQuery=1&sqlQuery=...&showQuery=...&showLinks=...')
 	// If so, the parameter 'customQuery=1' will be set:
@@ -65,9 +76,9 @@
 
 			$showRows = $_REQUEST['showRows']; // extract the $showRows parameter
 
-			$displayType = $_REQUEST['submit']; // extract the type of display requested by the user (either 'Display', 'Export' or '')
-			$exportFormat = $_REQUEST['exportFormatSelector']; // get the export format chosen by the user (only occurs in 'extract.php' form and in query result lists)
-			$exportOrder = $_REQUEST['exportOrder']; // get the export sort order chosen by the user (only occurs in 'extract.php' form and in query result lists)
+			$displayType = $_REQUEST['submit']; // extract the type of display requested by the user (either 'Display', 'Cite' or '')
+			$citeStyle = $_REQUEST['citeStyleSelector']; // get the cite style chosen by the user (only occurs in 'extract.php' form and in query result lists)
+			$citeOrder = $_REQUEST['citeOrder']; // get the citation sort order chosen by the user (only occurs in 'extract.php' form and in query result lists)
 
 			$oldQuery = $_REQUEST['oldQuery']; // get the query URL of the formerly displayed results page (if available) so that its's available on the subsequent receipt page that follows any add/edit/delete action!
 			if (ereg('sqlQuery%3D', $oldQuery)) // if '$oldQuery' still contains URL encoded data... ('%3D' is the URL encoded form of '=', see note below!)
@@ -79,12 +90,12 @@
 	else // if there was no previous SQL query provide the default one:
 		{
 			$sqlQuery = "SELECT author, title, year, publication, volume, pages FROM refs WHERE year &gt; 2001 ORDER BY year DESC, author";
-			$checkQuery = " checked";
+			$checkQuery = "";
 			$checkLinks = " checked";
 			$showRows = "10";
 			$displayType = ""; // ('' will produce the default columnar output style)
-			$exportFormat = "";
-			$exportOrder = "";
+			$citeStyle = "";
+			$citeOrder = "";
 			$oldQuery = "";
 		}
 
@@ -93,29 +104,48 @@
 
 	// (2a) Display header:
 	// call the 'displayHTMLhead()' and 'showPageHeader()' functions (which are defined in 'header.inc.php'):
-	displayHTMLhead(htmlentities($officialDatabaseName) . " -- SQL Search", "index,follow", "Search the " . htmlentities($officialDatabaseName), "", false, "");
+	displayHTMLhead(htmlentities($officialDatabaseName) . " -- SQL Search", "index,follow", "Search the " . htmlentities($officialDatabaseName), "", false, "", $viewType);
 	showPageHeader($HeaderString, $loginWelcomeMsg, $loginStatus, $loginLinks, $oldQuery);
 
 	// (2b) Start <form> and <table> holding the form elements:
 	echo "\n<form action=\"search.php\" method=\"POST\">";
 	echo "\n<input type=\"hidden\" name=\"formType\" value=\"sqlSearch\">"
 			. "\n<input type=\"hidden\" name=\"submit\" value=\"$displayType\">"
-			. "\n<input type=\"hidden\" name=\"exportFormatSelector\" value=\"" . rawurlencode($exportFormat) . "\">"
-			. "\n<input type=\"hidden\" name=\"exportOrder\" value=\"$exportOrder\">"
+			. "\n<input type=\"hidden\" name=\"citeStyleSelector\" value=\"" . rawurlencode($citeStyle) . "\">"
+			. "\n<input type=\"hidden\" name=\"citeOrder\" value=\"$citeOrder\">"
 			. "\n<input type=\"hidden\" name=\"oldQuery\" value=\"" . rawurlencode($oldQuery) . "\">";
 	echo "\n<table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"10\" width=\"95%\" summary=\"This table holds the search form\">"
 			. "\n<tr>\n\t<td width=\"58\" valign=\"top\"><b>SQL Query:</b></td>\n\t<td width=\"10\">&nbsp;</td>"
 			. "\n\t<td><textarea name=\"sqlQuery\" rows=\"6\" cols=\"60\">$sqlQuery</textarea></td>"
 			. "\n</tr>"
-			. "\n<tr>\n\t<td valign=\"top\"><b>Output Options:</b></td>\n\t<td>&nbsp;</td>"
+			. "\n<tr>\n\t<td valign=\"top\"><b>Display Options:</b></td>\n\t<td>&nbsp;</td>"
 			. "\n\t<td valign=\"top\"><input type=\"checkbox\" name=\"showLinks\" value=\"1\"$checkLinks>&nbsp;&nbsp;&nbsp;Display Links"
 			. "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Show&nbsp;&nbsp;&nbsp;<input type=\"text\" name=\"showRows\" value=\"$showRows\" size=\"4\">&nbsp;&nbsp;&nbsp;records per page</td>"
 			. "\n</tr>"
 			. "\n<tr>\n\t<td>&nbsp;</td>\n\t<td>&nbsp;</td>"
-			. "\n\t<td valign=\"top\"><input type=\"checkbox\" name=\"showQuery\" value=\"1\"$checkQuery>&nbsp;&nbsp;&nbsp;Display SQL query</td>"
+			. "\n\t<td valign=\"top\"><input type=\"checkbox\" name=\"showQuery\" value=\"1\"$checkQuery>&nbsp;&nbsp;&nbsp;Display SQL query"
+			. "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;View type:&nbsp;&nbsp;"
+			. "\n\t\t<select name=\"viewType\">"
+			. "\n\t\t\t<option>Web</option>"
+			. "\n\t\t\t<option>Print</option>"
+			. "\n\t\t</select>"
+			. "\n\t</td>"
 			. "\n</tr>"
-			. "\n<tr>\n\t<td>&nbsp;</td>\n\t<td>&nbsp;</td>"
-			. "\n\t<td><br><input type=\"submit\" value=\"Search\"></td>"
+			. "\n<tr>\n\t<td>&nbsp;</td>\n\t<td>&nbsp;</td>";
+
+	if (isset($_SESSION['user_permissions']) AND ereg("allow_sql_search", $_SESSION['user_permissions'])) // if the 'user_permissions' session variable contains 'allow_sql_search'...
+	// adjust the title string for the search button
+	{
+		$sqlSearchButtonLock = "";
+		$sqlSearchTitle = "search the database using the above query & display options";
+	}
+	else // Note, that disabling the submit button is just a cosmetic thing -- the user can still submit the form by pressing enter or by building the correct URL from scratch!
+	{
+		$sqlSearchButtonLock = " disabled";
+		$sqlSearchTitle = "not available since you have no permission to perform custom SQL searches";
+	}
+
+	echo "\n\t<td><br><input type=\"submit\" value=\"Search\"$sqlSearchButtonLock title=\"$sqlSearchTitle\"></td>"
 			. "\n</tr>"
 			. "\n<tr>\n\t<td align=\"center\" colspan=\"3\">&nbsp;</td>"
 			. "\n</tr>"
@@ -139,5 +169,6 @@
 
 	// --------------------------------------------------------------------
 ?>
+
 </body>
 </html> 
