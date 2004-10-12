c7bab447fe47450abe0e68bdfc5bc814dc45cc1c
wendebaum <wendebaum@47a94560-c220-0410-a5a2-efdbd790cbb4>
2004-10-12 08:38:41 +0000

    - file header included

--- trunk/refbase/code/php/locales/core.php
+++ trunk/refbase/code/php/locales/core.php
@@ -1,9 +1,17 @@
 <?php
-$f = $refbaseDir ."locales/".$locale."/common.inc";
-//echo $f;
-ob_start();
-	readfile( $f );
-	$s = "\$loc=array(".ob_get_contents().");";
-	eval( $s );
-ob_end_clean();
+	// Project:    Web Reference Database (refbase) <http://www.refbase.net>
+	// Copyright:  Jochen Wendebaum <mailto:refbase@extracts.de>
+	//             This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+	//             Please see the GNU General Public License for more details.
+	// File:       ./locales/core.php
+	// Created:    01-Oct-04, 12:00
+	// Modified:   12-Oct-04, 12:00
+	
+  $f = $refbaseDir ."locales/".$locale."/common.inc"; // get filename
+  
+  ob_start();
+  	readfile( $f ); // read the file contents
+  	$s = "\$loc=array(".ob_get_contents().");"; 
+  	eval( $s );    // ...and store everything into $loc
+  ob_end_clean();
 ?>
