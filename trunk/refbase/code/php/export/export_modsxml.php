c8f640451f1a7eb1150c1baab480d5a4958bb7f0
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2004-10-13 19:27:33 +0000

    - moved function 'exportRecord()' from 'code/inc/styles/MODS_XML.inc.php'   to this file and renamed it to 'exportRecords()' since it will now export   all found records _at once_ (it was exporting each record individually   before)

---
+++ trunk/refbase/code/php/export/export_modsxml.php
@@ -0,0 +1,36 @@
+<?php
+	// Project:    Web Reference Database (refbase) <http://www.refbase.net>
+	// Copyright:  Matthias Steffens <mailto:refbase@extracts.de> and the file's
+	//             original author.
+	//
+	//             This code is distributed in the hope that it will be useful,
+	//             but WITHOUT ANY WARRANTY.  Please see the GNU General Public
+	//             License for more details.
+	//
+	// File:       ./export/export_modsxml.php
+	// Author:     Richard Karnesky <mailto:karnesky@northwestern.edu>
+	//
+	// Created:    02-Oct-04, 12:00
+	// Modified:   12-Oct-04, 23:16
+
+	// This exports MODS XML. This file must reside in the 'export' directory of the refbase root directory.
+	// It uses functions from include file 'modsxml.inc.php' that requires the ActiveLink PHP XML Package,
+	// which is available under the GPL from: <http://www.active-link.com/software/>
+	
+	// --------------------------------------------------------------------
+
+	// --- BEGIN EXPORT FORMAT ---
+
+	// Export found records as MODS XML:
+	function exportRecords($result, $exportFormat)
+	{
+		// Generate and serve a MODS XML file of ALL records:
+		$recordCollection = modsCollection($result); // function 'modsCollection()' is defined in 'modsxml.inc.php'
+
+		return $recordCollection;
+	}
+
+	// --- BEGIN EXPORT FORMAT ---
+
+	// --------------------------------------------------------------------
+?>
