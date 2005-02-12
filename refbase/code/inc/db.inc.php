98d281aea4f8b1743b0ec9723bcc4a1163b8ea35
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2005-02-12 18:11:55 +0000

    - changed wording in comment slightly

--- refbase/code/inc/db.inc.php
+++ refbase/code/inc/db.inc.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./initialize/db.inc.php
 	// Created:    15-Oct-02, 19:11
-	// Modified:   28-Feb-04, 22:06
+	// Modified:   12-Feb-05, 19:09
 
 	// This file holds crucial
 	// database access information.
@@ -21,8 +21,8 @@
 	//        we highly recommend not to use the default values!)
 	// CAUTION: To avoid security risks you must not permit any remote user to view this file!
 	//          E.g., this can be done by adjusting the config file of your server ("httpd.conf"
-	//          in case of the apache web server) to disallow viewing of "*\.inc" files
-	//          ("Deny from all"). Please see the refbase readme for further information.
+	//          in case of the Apache web server) to disallow viewing of "*\.inc.php" files
+	//          ("Deny from all"). Please see your web server documentation for further information.
 
 	// --------------------------------------------------------------------
 
