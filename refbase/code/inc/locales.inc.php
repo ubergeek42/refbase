1fc356b81ff1cab75c5ed9dff04b4c894d86a496
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2005-04-02 11:52:14 +0000

    standardized file header & whitespace

--- refbase/code/inc/locales.inc.php
+++ refbase/code/inc/locales.inc.php
@@ -1,18 +1,23 @@
 <?php
 	// Project:    Web Reference Database (refbase) <http://www.refbase.net>
-	// Copyright:  Jochen Wendebaum <mailto:refbase@extracts.de>
-	//             This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
-	//             Please see the GNU General Public License for more details.
-	// File:       ./locales/core.php
+	// Copyright:  Matthias Steffens <mailto:refbase@extracts.de> and the file's
+	//             original author.
+	//
+	//             This code is distributed in the hope that it will be useful,
+	//             but WITHOUT ANY WARRANTY.  Please see the GNU General Public
+	//             License for more details.
+	//
+	// File:       ./includes/locales.inc.php
+	// Author:     Jochen Wendebaum <mailto:wendebaum@users.sourceforge.net>
+	//
 	// Created:    12-Oct-04, 12:00
-	// Modified:   12-Oct-04, 12:00
+	// Modified:   02-Apr-05, 13:41
 
 	// This is the locales include file.
-	// It contains functions that read the locales depending on the personal settings of the 
-	// user or the default language, if no personal information can be found
+	// It contains functions that read the locales depending on the personal settings
+	// of the user or the default language, if no personal information can be found.
 
-  $locale = $defaultLanguage; // todo: get the personal language information
+	$locale = $defaultLanguage; // todo: get the personal language information
 
-  include 'locales/core.php'; // include the locales
-
+	include 'locales/core.php'; // include the locales
 ?>
