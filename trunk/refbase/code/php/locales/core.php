406dd17c0400895ba20b44673e95a990316f68a4
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2005-04-02 11:52:14 +0000

    standardized file header & whitespace

--- trunk/refbase/code/php/locales/core.php
+++ trunk/refbase/code/php/locales/core.php
@@ -1,17 +1,23 @@
 <?php
 	// Project:    Web Reference Database (refbase) <http://www.refbase.net>
-	// Copyright:  Jochen Wendebaum <mailto:refbase@extracts.de>
-	//             This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
-	//             Please see the GNU General Public License for more details.
+	// Copyright:  Matthias Steffens <mailto:refbase@extracts.de> and the file's
+	//             original author.
+	//
+	//             This code is distributed in the hope that it will be useful,
+	//             but WITHOUT ANY WARRANTY.  Please see the GNU General Public
+	//             License for more details.
+	//
 	// File:       ./locales/core.php
+	// Author:     Jochen Wendebaum <mailto:wendebaum@users.sourceforge.net>
+	//
 	// Created:    01-Oct-04, 12:00
-	// Modified:   12-Oct-04, 12:00
-	
-  $f = "locales/".$locale."/common.inc"; // get filename
-  
-  ob_start();
-  	readfile( $f ); // read the file contents
-  	$s = "\$loc=array(".ob_get_contents().");"; 
-  	eval( $s );    // ...and store everything into $loc
-  ob_end_clean();
+	// Modified:   02-Apr-05, 13:43
+
+	$f = "locales/".$locale."/common.inc"; // get filename
+
+	ob_start();
+		readfile( $f ); // read the file contents
+		$s = "\$loc=array(".ob_get_contents().");"; 
+		eval( $s );    // ...and store everything into $loc
+	ob_end_clean();
 ?>
