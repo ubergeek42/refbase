052e7a656f265018acadb111d0456acad90ec379
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-06-12 23:39:23 +0000

    the include files 'execute.inc.php' and 'export.inc.php' are now included in 'search.php' which is why the include calls were removed from 'export_xml2*' files

--- trunk/refbase/code/php/export/bibutils/export_xml2bib.php
+++ trunk/refbase/code/php/export/bibutils/export_xml2bib.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./export/bibutils/export_xml2bib.php
 	// Created:    28-Sep-04, 22:14
-	// Modified:   09-May-06, 16:42
+	// Modified:   31-May-06, 01:45
 
 	// This is an export format file (which must reside within the 'export/' sub-directory of your refbase root directory). It contains a version of the
 	// 'exportRecords()' function that outputs records according to the export format used by 'BibTeX', the bibliographic companion to the LaTeX macro package.
@@ -21,8 +21,6 @@
 	//    - bibutils <http://www.scripps.edu/~cdputnam/software/bibutils/bibutils.html>
 	//    - ActiveLink PHP XML Package <http://www.active-link.com/software/>
 
-	include 'includes/execute.inc.php';
-	include 'includes/export.inc.php';
 	function exportRecords($result, $rowOffset, $showRows, $exportStylesheet, $displayType)
 	{
 		// function 'exportBibutils()' is defined in 'execute.inc.php'
