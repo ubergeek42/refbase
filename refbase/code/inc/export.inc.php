55b92d25ac7c168f92fae89b3783070e649e79a3
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-06-12 23:42:34 +0000

    the search & replace patterns which convert refbase markup to LaTeX/BibTeX markup & entities were moved to a separate include file ('transtab_refbase_bibtex.inc.php')

--- refbase/code/inc/export.inc.php
+++ refbase/code/inc/export.inc.php
@@ -9,11 +9,13 @@
 	//
 	// File:       ./includes/export.inc.php
 	// Created:    09-May-06, 15:34
-	// Modified:   10-May-06, 00:44
+	// Modified:   31-May-06, 01:50
 
 	// This file contains functions
 	// that are used when exporting
 	// records from the database.
+
+	include 'includes/transtab_refbase_bibtex.inc.php'; // include refbase markup -> BibTeX search & replace patterns
 
 	// --------------------------------------------------------------------
 
@@ -21,70 +23,13 @@
 	// converts any contained refbase markup into proper LaTeX/BibTeX markup:
 	function standardizeBibtexOutput($bibtexSourceText)
 	{
-		// We convert refbase fontshape markup (italic, bold) into LaTeX commands of the 'textcomp' package,
-		// super- and subscript as well as greek symbols get converted into the respective commands in math mode.
-		// You may need to adopt the LaTeX markup to something that suits your individual needs:
-		//									"refbase markup"       =>  'LaTeX markup'
-		$markupSearchReplacePatterns = array("/\\\\_(.+?)\\\\_/"   =>  '\\textit{\\1}',  // or use '\\it{\\1}' (the backslashes before the substrings were inserted by bibutils: '_word_' gets '\_word\_')
-											"/\\*\\*(.+?)\\*\\*/"  =>  '\\textbf{\\1}',  // or use '\\bf{\\1}'
-											"/\\[super:(.+?)\\]/i" =>  '$^{\\1}$', // or use '\\textsuperscript{\\1}'
-											"/\\[sub:(.+?)\\]/i"   =>  '$_{\\1}$', // or use '\\textsubscript{\\1}' if defined in your package
-											"/\\[permil\\]/"       =>  '{\\textperthousand}',
-											"/\\[infinity\\]/"     =>  '$\\infty$',
-											"/\\[alpha\\]/"        =>  '$\\alpha$',
-											"/\\[beta\\]/"         =>  '$\\beta$',
-											"/\\[gamma\\]/"        =>  '$\\gamma$',
-											"/\\[delta\\]/"        =>  '$\\delta$',
-											"/\\[epsilon\\]/"      =>  '$\\epsilon$',
-											"/\\[zeta\\]/"         =>  '$\\zeta$',
-											"/\\[eta\\]/"          =>  '$\\eta$',
-											"/\\[theta\\]/"        =>  '$\\theta$',
-											"/\\[iota\\]/"         =>  '$\\iota$',
-											"/\\[kappa\\]/"        =>  '$\\kappa$',
-											"/\\[lambda\\]/"       =>  '$\\lambda$',
-											"/\\[mu\\]/"           =>  '$\\mu$',
-											"/\\[nu\\]/"           =>  '$\\nu$',
-											"/\\[xi\\]/"           =>  '$\\xi$',
-											"/\\[omicron\\]/"      =>  '$o$',
-											"/\\[pi\\]/"           =>  '$\\pi$',
-											"/\\[rho\\]/"          =>  '$\\rho$',
-											"/\\[sigmaf\\]/"       =>  '$\\varsigma$',
-											"/\\[sigma\\]/"        =>  '$\\sigma$',
-											"/\\[tau\\]/"          =>  '$\\tau$',
-											"/\\[upsilon\\]/"      =>  '$\\upsilon$',
-											"/\\[phi\\]/"          =>  '$\\phi$',
-											"/\\[chi\\]/"          =>  '$\\chi$',
-											"/\\[psi\\]/"          =>  '$\\psi$',
-											"/\\[omega\\]/"        =>  '$\\omega$',
-											"/\\[Alpha\\]/"        =>  '$A$',
-											"/\\[Beta\\]/"         =>  '$B$',
-											"/\\[Gamma\\]/"        =>  '$\\Gamma$',
-											"/\\[Delta\\]/"        =>  '$\\Delta$',
-											"/\\[Epsilon\\]/"      =>  '$E$',
-											"/\\[Zeta\\]/"         =>  '$Z$',
-											"/\\[Eta\\]/"          =>  '$H$',
-											"/\\[Theta\\]/"        =>  '$\\Theta$',
-											"/\\[Iota\\]/"         =>  '$I$',
-											"/\\[Kappa\\]/"        =>  '$K$',
-											"/\\[Lambda\\]/"       =>  '$\\Lambda$',
-											"/\\[Mu\\]/"           =>  '$M$',
-											"/\\[Nu\\]/"           =>  '$N$',
-											"/\\[Xi\\]/"           =>  '$\\Xi$',
-											"/\\[Omicron\\]/"      =>  '$O$',
-											"/\\[Pi\\]/"           =>  '$\\Pi$',
-											"/\\[Rho\\]/"          =>  '$R$',
-											"/\\[Sigma\\]/"        =>  '$\\Sigma$',
-											"/\\[Tau\\]/"          =>  '$T$',
-											"/\\[Upsilon\\]/"      =>  '$\\Upsilon$',
-											"/\\[Phi\\]/"          =>  '$\\Phi$',
-											"/\\[Chi\\]/"          =>  '$X$',
-											"/\\[Psi\\]/"          =>  '$\\Psi$',
-											"/\\[Omega\\]/"        =>  '$\\Omega$',
-											"/^(?=(URL|LOCATION|NOTE|KEYWORDS)=)/mi" =>  'opt');
+		// The array '$transtab_refbase_bibtex' contains search & replace patterns for conversion from refbase markup to LaTeX/BibTeX markup & entities.
+		// Converts refbase fontshape markup (italic, bold) into LaTeX commands of the 'textcomp' package, super- and subscript as well as greek
+		// symbols get converted into the respective commands in math mode. You may need to adopt the LaTeX markup to suit your individual needs.
+		global $transtab_refbase_bibtex; // defined in 'transtab_refbase_bibtex.inc.php'
 
-		// Perform above search & replace actions on the given BibTeX text:
-		$bibtexSourceText = searchReplaceText($markupSearchReplacePatterns, $bibtexSourceText, true); // function 'searchReplaceText()' is defined in 'include.inc.php'
-
+		// Perform search & replace actions on the given BibTeX text:
+		$bibtexSourceText = searchReplaceText($transtab_refbase_bibtex, $bibtexSourceText, true); // function 'searchReplaceText()' is defined in 'include.inc.php'
 
 		return $bibtexSourceText;
 	}
