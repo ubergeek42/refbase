04b1ca31da1b9b3e89c3f3b97f6e2545b6e0e3c5
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-06-12 23:32:51 +0000

    we'll now HTML encode higher ASCII characters in locales

--- trunk/refbase/code/php/locales/core.php
+++ trunk/refbase/code/php/locales/core.php
@@ -11,7 +11,7 @@
 	// Author:     Jochen Wendebaum <mailto:wendebaum@users.sourceforge.net>
 	//
 	// Created:    01-Oct-04, 12:00
-	// Modified:   02-Apr-05, 13:43
+	// Modified:   05-Jun-06, 16:06
 
 	$f = "locales/".$locale."/common.inc"; // get filename
 
@@ -20,4 +20,13 @@
 		$s = "\$loc=array(".ob_get_contents().");"; 
 		eval( $s );    // ...and store everything into $loc
 	ob_end_clean();
+
+	// HTML encode higher ASCII characters in locales:
+	foreach ($loc as $locKey => $locValue)
+	{
+		$loc[$locKey] = encodeHTML($locValue); // function 'encodeHTML()' is defined in 'include.inc.php'
+
+		if (preg_match("/&lt;a href=&quot;.+?&quot;&gt;.+?&lt;\/a&gt;/", $loc[$locKey])) // dirty hack to allow URLs within (otherwise HTML encoded) locales
+			$loc[$locKey] = preg_replace("/&lt;a href=&quot;(.+?)&quot;&gt;(.+?)&lt;\/a&gt;/", "<a href=\"\\1\">\\2</a>", $loc[$locKey]);
+	}
 ?>
