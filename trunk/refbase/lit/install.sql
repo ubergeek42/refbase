55ae4cd4b77b6b4c32914ad4fe7b2982bb76d433
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-06-13 11:35:54 +0000

    * updated table definition for table 'formats' to include 'cite' as new format type * table 'formats': changed character case for 'Bibtex' to 'BibTeX' * added a table row for "ODF XML" export format to table 'formats' * added table rows for citation formats html, RTF, PDF, LaTeX, Markdown & ASCII to   table 'formats' * table 'styles': changed path specs for cite styles to their new location (in   'cite/styles/')

--- trunk/refbase/lit/install.sql
+++ trunk/refbase/lit/install.sql
@@ -4,7 +4,7 @@
 #             Please see the GNU General Public License for more details.
 # File:       ./install.sql
 # Created:    02-Oct-04, 20:11
-# Modified:   04-Mar-06, 02:21
+# Modified:   10-Jun-06, 22:19
 
 # MySQL database structure & initial data (for use with 'latin1' character set)
 
@@ -135,14 +135,14 @@ DROP TABLE IF EXISTS `formats`;
 CREATE TABLE `formats` (
   `format_id` mediumint(8) unsigned NOT NULL auto_increment,
   `format_name` varchar(100) default NULL,
-  `format_type` enum('export','import') NOT NULL default 'export',
+  `format_type` enum('export','import','cite') NOT NULL default 'export',
   `format_enabled` enum('true','false') NOT NULL default 'true',
   `format_spec` varchar(255) default NULL,
   `order_by` varchar(25) default NULL,
   `depends_id` mediumint(8) unsigned NOT NULL default '0',
   PRIMARY KEY  (`format_id`),
   KEY `format_name` (`format_name`)
-) TYPE=MyISAM AUTO_INCREMENT=17 ;
+) TYPE=MyISAM AUTO_INCREMENT=24 ;
 
 #
 # data for table `formats`
@@ -151,8 +151,8 @@ CREATE TABLE `formats` (
 INSERT INTO `formats` VALUES (1, 'MODS XML', 'import', 'true', 'bibutils/import_modsxml2refbase.php', '06', 2), 
 (2, 'MODS XML', 'export', 'true', 'export_modsxml.php', '06', 1), 
 (3, 'Text (CSV)', 'export', 'false', 'export_textcsv.php', '07', 1), 
-(4, 'Bibtex', 'import', 'true', 'bibutils/import_bib2refbase.php', '01', 2), 
-(5, 'Bibtex', 'export', 'true', 'bibutils/export_xml2bib.php', '01', 2), 
+(4, 'BibTeX', 'import', 'true', 'bibutils/import_bib2refbase.php', '01', 2), 
+(5, 'BibTeX', 'export', 'true', 'bibutils/export_xml2bib.php', '01', 2), 
 (6, 'Endnote', 'import', 'true', 'bibutils/import_end2refbase.php', '02', 2), 
 (7, 'Endnote', 'export', 'true', 'bibutils/export_xml2end.php', '02', 2), 
 (8, 'Pubmed Medline', 'import', 'true', 'import_medline2refbase.php', '08', '1'),
@@ -163,7 +163,14 @@ INSERT INTO `formats` VALUES (1, 'MODS XML', 'import', 'true', 'bibutils/import_
 (13, 'CSA', 'import', 'true', 'import_csa2refbase.php', '05', '1'),
 (14, 'Copac', 'import', 'true', 'bibutils/import_copac2refbase.php', '10', '2'),
 (15, 'SRW XML', 'export', 'true', 'export_srwxml.php', '11', 1), 
-(16, 'OpenSearch RSS', 'export', 'true', 'export_osrss.php', '12', 1);
+(16, 'ODF XML', 'export', 'true', 'export_odfxml.php', '12', 1), 
+(17, 'OpenSearch RSS', 'export', 'true', 'export_osrss.php', '13', 1),
+(18, 'html', 'cite', 'true', 'formats/cite_html.php', '14', '1'),
+(19, 'RTF', 'cite', 'true', 'formats/cite_rtf.php', '15', '1'),
+(20, 'PDF', 'cite', 'true', 'formats/cite_pdf.php', '16', '1'),
+(21, 'LaTeX', 'cite', 'true', 'formats/cite_latex.php', '17', '1'),
+(22, 'Markdown', 'cite', 'true', 'formats/cite_markdown.php', '18', '1'),
+(23, 'ASCII', 'cite', 'true', 'formats/cite_ascii.php', '19', '1');
 
 # --------------------------------------------------------
 
@@ -327,13 +334,13 @@ CREATE TABLE `styles` (
 # data for table `styles`
 #
 
-INSERT INTO `styles` VALUES (1, 'Polar Biol', 'true', 'cite_PolarBiol_MarBiol_MEPS.php', '1', 1),
-(2, 'Mar Biol', 'true', 'cite_PolarBiol_MarBiol_MEPS.php', '2', 1),
-(3, 'MEPS', 'true', 'cite_PolarBiol_MarBiol_MEPS.php', '3', 1),
-(4, 'Deep Sea Res', 'true', 'cite_DeepSeaRes.php', '4', 1),
-(5, 'Ann Glaciol', 'true', 'cite_AnnGlaciol_JGlaciol.php', '5', 1),
-(6, 'J Glaciol', 'true', 'cite_AnnGlaciol_JGlaciol.php', '6', 1),
-(7, 'Text Citation', 'true', 'cite_TextCitation.php', '7', 1);
+INSERT INTO `styles` VALUES (1, 'Polar Biol', 'true', 'styles/cite_PolarBiol_MarBiol_MEPS.php', '1', 1),
+(2, 'Mar Biol', 'true', 'styles/cite_PolarBiol_MarBiol_MEPS.php', '2', 1),
+(3, 'MEPS', 'true', 'styles/cite_PolarBiol_MarBiol_MEPS.php', '3', 1),
+(4, 'Deep Sea Res', 'true', 'styles/cite_DeepSeaRes.php', '4', 1),
+(5, 'Ann Glaciol', 'true', 'styles/cite_AnnGlaciol_JGlaciol.php', '5', 1),
+(6, 'J Glaciol', 'true', 'styles/cite_AnnGlaciol_JGlaciol.php', '6', 1),
+(7, 'Text Citation', 'true', 'styles/cite_TextCitation.php', '7', 1);
 
 # --------------------------------------------------------
 
