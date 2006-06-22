2365dd51147ade57885b635c04f871680d5966c0
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-06-22 22:20:42 +0000

    just fixed a typo in header comment

--- refbase/code/php/export/export_odfxml.php
+++ refbase/code/php/export/export_odfxml.php
@@ -5,10 +5,10 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./export/export_odfxml.php
 	// Created:    01-Jun-06, 13:57
-	// Modified:   01-Jun-06, 13:58
+	// Modified:   13-Jun-06, 00:23
 
 	// This exports ODF XML. This file must reside in the 'export' directory of the refbase root directory.
-	// It uses functions from include file 'modsxml.inc.php' that requires the ActiveLink PHP XML Package,
+	// It uses functions from include file 'odfxml.inc.php' that requires the ActiveLink PHP XML Package,
 	// which is available under the GPL from: <http://www.active-link.com/software/>
 	
 	// --------------------------------------------------------------------
