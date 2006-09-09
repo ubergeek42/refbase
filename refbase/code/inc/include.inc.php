311d1ecb8161e21c8035afc3a02dca6d3cdf3b32
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-09-09 19:35:58 +0000

    * functions 'extractFormElementsRefineDisplay()' and 'verifySQLQuery()': when   displaying links, the 'type' field is now always included in the SQL query; this   allows for proper generation of OpenURL links and COinS span elements in list view * added function 'setHTTPStatus()' which sets the HTTP status response

--- refbase/code/inc/include.inc.php
+++ refbase/code/inc/include.inc.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./includes/include.inc.php
 	// Created:    16-Apr-02, 10:54
-	// Modified:   07-Sep-06, 12:00
+	// Modified:   09-Sep-06, 15:14
 
 	// This file contains important
 	// functions that are shared
@@ -1629,7 +1629,7 @@ EOF;
 																				// (which is required in order to obtain unique checkbox names)
 
 				if ($showLinks == "1")
-					$query = eregi_replace(" FROM $tableRefs",", file, url, doi, isbn FROM $tableRefs",$query); // add 'file', 'url', 'doi' & 'isbn' columns
+					$query = eregi_replace(" FROM $tableRefs",", file, url, doi, isbn, type FROM $tableRefs",$query); // add 'file', 'url', 'doi', 'isbn' & 'type' columns
 			}
 		}
 		elseif ($queryTable == $tableUsers) // 'users.php':
@@ -3655,6 +3655,61 @@ EOF;
 
 	// --------------------------------------------------------------------
 
+	// Set HTTP status response
+
+	// From user 'Ciantic' found at <http://www.php.net/header> (24-Dec-2005 03:07)
+	// This contains all HTTP status responses defined in RFC2616 section 6.1.1
+	// See <http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.1.1>
+	function setHTTPStatus($statusCode)
+	{
+		// HTTP Protocol defined status codes:
+		static $http = array (
+								100 => "HTTP/1.1 100 Continue",
+								101 => "HTTP/1.1 101 Switching Protocols",
+								200 => "HTTP/1.1 200 OK",
+								201 => "HTTP/1.1 201 Created",
+								202 => "HTTP/1.1 202 Accepted",
+								203 => "HTTP/1.1 203 Non-Authoritative Information",
+								204 => "HTTP/1.1 204 No Content",
+								205 => "HTTP/1.1 205 Reset Content",
+								206 => "HTTP/1.1 206 Partial Content",
+								300 => "HTTP/1.1 300 Multiple Choices",
+								301 => "HTTP/1.1 301 Moved Permanently",
+								302 => "HTTP/1.1 302 Found",
+								303 => "HTTP/1.1 303 See Other",
+								304 => "HTTP/1.1 304 Not Modified",
+								305 => "HTTP/1.1 305 Use Proxy",
+								307 => "HTTP/1.1 307 Temporary Redirect",
+								400 => "HTTP/1.1 400 Bad Request",
+								401 => "HTTP/1.1 401 Unauthorized",
+								402 => "HTTP/1.1 402 Payment Required",
+								403 => "HTTP/1.1 403 Forbidden",
+								404 => "HTTP/1.1 404 Not Found",
+								405 => "HTTP/1.1 405 Method Not Allowed",
+								406 => "HTTP/1.1 406 Not Acceptable",
+								407 => "HTTP/1.1 407 Proxy Authentication Required",
+								408 => "HTTP/1.1 408 Request Time-out",
+								409 => "HTTP/1.1 409 Conflict",
+								410 => "HTTP/1.1 410 Gone",
+								411 => "HTTP/1.1 411 Length Required",
+								412 => "HTTP/1.1 412 Precondition Failed",
+								413 => "HTTP/1.1 413 Request Entity Too Large",
+								414 => "HTTP/1.1 414 Request-URI Too Large",
+								415 => "HTTP/1.1 415 Unsupported Media Type",
+								416 => "HTTP/1.1 416 Requested range not satisfiable",
+								417 => "HTTP/1.1 417 Expectation Failed",
+								500 => "HTTP/1.1 500 Internal Server Error",
+								501 => "HTTP/1.1 501 Not Implemented",
+								502 => "HTTP/1.1 502 Bad Gateway",
+								503 => "HTTP/1.1 503 Service Unavailable",
+								504 => "HTTP/1.1 504 Gateway Time-out"
+		);
+
+		header($http[$statusCode]);
+	}
+
+	// --------------------------------------------------------------------
+
 	// Convert to character encoding:
 	// This function converts text that's represented in the refbase database encoding
 	// (which is indicated in '$contentTypeCharset') into the character encoding given
@@ -3902,7 +3957,7 @@ EOF;
 			$sqlQuery = eregi_replace(" FROM $tableRefs",", serial FROM $tableRefs",$sqlQuery); // add 'serial' column (which is required in order to obtain unique checkbox names)
 
 			if ($showLinks == "1")
-				$sqlQuery = eregi_replace(" FROM $tableRefs",", file, url, doi, isbn FROM $tableRefs",$sqlQuery); // add 'file', 'url', 'doi' & 'isbn' columns
+				$sqlQuery = eregi_replace(" FROM $tableRefs",", file, url, doi, isbn, type FROM $tableRefs",$sqlQuery); // add 'file', 'url', 'doi', 'isbn' & 'type' columns
 		}
 
 		// fix escape sequences within the SQL query:
