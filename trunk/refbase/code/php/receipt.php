e61c5c43e886e76fb2b4ee1b373fb983ca580d82
msteffens <msteffens@47a94560-c220-0410-a5a2-efdbd790cbb4>
2006-09-30 00:31:17 +0000

    * the stripping of slashes is now handled by function 'stripSlashesIfMagicQuotes()' * quote_smart()ed the '$serialNo' variable in SQL queries

--- trunk/refbase/code/php/receipt.php
+++ trunk/refbase/code/php/receipt.php
@@ -5,7 +5,7 @@
 	//             Please see the GNU General Public License for more details.
 	// File:       ./receipt.php
 	// Created:    02-Jan-03, 22:43
-	// Modified:   26-Sep-06, 17:15
+	// Modified:   29-Sep-06, 21:07
 
 	// This php script will display a feedback page after any action of
 	// adding/editing/deleting a record. It will display links to the
@@ -83,7 +83,8 @@
 
 	// Extract generic variables from the request:
 	$oldQuery = $_REQUEST['oldQuery']; // fetch the query URL of the formerly displayed results page so that its's available on the subsequent receipt page that follows any add/edit/delete action!
-	$oldQuery = str_replace('\"','"',$oldQuery); // replace any \" with "
+	$oldQuery = stripSlashesIfMagicQuotes($oldQuery); // function 'stripSlashesIfMagicQuotes()' is defined in 'include.inc.php'
+//	$oldQuery = str_replace('\"','"',$oldQuery); // replace any \" with "
 
 	// --------------------------------------------------------------------
 
@@ -101,14 +102,12 @@
 
 	// (4b) DISPLAY results:
 	// First, construct the correct sql query that will link back to the added/edited record:
-	//
-	// TODO: quote_smart()?
 	if (isset($_SESSION['loginEmail'])) // if a user is logged in, show user specific fields:
 		$sqlQuery = "SELECT author, title, type, year, publication, abbrev_journal, volume, issue, pages, corporate_author, thesis, address, keywords, abstract, publisher, place, editor, language, summary_language, orig_title, series_editor, series_title, abbrev_series_title, series_volume, series_issue, edition, issn, isbn, medium, area, expedition, conference, notes, approved, location, call_number, serial, marked, copy, selected, user_keys, user_notes, user_file, user_groups, cite_key, related"
-				. " FROM $tableRefs LEFT JOIN $tableUserData ON serial = record_id AND user_id =" . quote_smart($loginUserID) . " WHERE serial RLIKE \"^(" . $serialNo . ")$\" ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
+				. " FROM $tableRefs LEFT JOIN $tableUserData ON serial = record_id AND user_id = " . quote_smart($loginUserID) . " WHERE serial RLIKE " . quote_smart("^(" . $serialNo . ")$") . " ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
 	else // if NO user logged in, don't display any user specific fields:
 		$sqlQuery = "SELECT author, title, type, year, publication, abbrev_journal, volume, issue, pages, corporate_author, thesis, address, keywords, abstract, publisher, place, editor, language, summary_language, orig_title, series_editor, series_title, abbrev_series_title, series_volume, series_issue, edition, issn, isbn, medium, area, expedition, conference, notes, approved, location, call_number, serial"
-				. " FROM $tableRefs WHERE serial RLIKE \"^(" . $serialNo . ")$\" ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
+				. " FROM $tableRefs WHERE serial RLIKE " . quote_smart("^(" . $serialNo . ")$") . " ORDER BY author, year DESC, publication"; // we simply use the fixed default ORDER BY clause here
 
 	$sqlQuery = rawurlencode($sqlQuery);
 
